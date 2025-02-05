57a3302922dc7dc62373b4e37b80f117c8aba589
abhi-kr07 <112303518+abhi-kr07@users.noreply.github.com>
2025-02-05 05:42:51 +0530

    Update updatefrontend.sh

--- IP_update_automation/updatefrontend.sh
+++ IP_update_automation/updatefrontend.sh
@@ -3,7 +3,7 @@
 Instance_ID="i-0e12f3c50299eb0ac"
 Public_IPv4=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
-find_the_file="../frontend/.env.docker"
+find_the_file="../frontend/.env.sample"
 current_url=$(cat $file_the_find)
 
 if [[ "${current_url}" != "VITE_API_PATH=\"http://${Public_IPv4}:31100\"" ]]
